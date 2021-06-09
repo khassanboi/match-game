9cf4b7fb22b9c2a83d6827949c4c0f3b3da7790d
khassanboi <khassanboi@gmail.com>
2021-06-09 11:27:11 +0500

    indentation fixes (tabs to spaces)

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 $(document).ready(() => {
-	//Registration form open-close
-	$("#register").click(() => {
-		$("#reg").addClass("active");
-	});
-	$("#cancel-reg").click(() => {
-		$("#reg").removeClass("active");
-	});
+  //Registration form open-close
+  $("#register").click(() => {
+    $("#reg").addClass("active");
+  });
+  $("#cancel-reg").click(() => {
+    $("#reg").removeClass("active");
+  });
 
-	//Registration form validation
-	$("#first-name").keydown(() => {
-		if (!$("#first-name").val()) {
-			$("#first-name").parent().removeClass("active");
-		} else {
-			$("#first-name").parent().addClass("active");
-		}
-	});
+  //Registration form validation
+  $("#first-name").keydown(() => {
+    if (!$("#first-name").val()) {
+      $("#first-name").parent().removeClass("active");
+    } else {
+      $("#first-name").parent().addClass("active");
+    }
+  });
 
-	$("#last-name").keydown(() => {
-		if (!$("#last-name").val()) {
-			$("#last-name").parent().removeClass("active");
-		} else {
-			$("#last-name").parent().addClass("active");
-		}
-	});
+  $("#last-name").keydown(() => {
+    if (!$("#last-name").val()) {
+      $("#last-name").parent().removeClass("active");
+    } else {
+      $("#last-name").parent().addClass("active");
+    }
+  });
 
-	$(".game__card").click((e) => {
-		$(e.currentTarget).addClass("turned");
-		setTimeout(() => {
-			$(e.currentTarget).removeClass("turned");
-		}, 2000);
-	});
+  $(".game__card").click((e) => {
+    $(e.currentTarget).addClass("turned");
+    setTimeout(() => {
+      $(e.currentTarget).removeClass("turned");
+    }, 2000);
+  });
 });
