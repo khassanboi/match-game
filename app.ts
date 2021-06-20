a226d8481a95811649b8f21db17172e11cdf0751
khassanboi <khassanboi@gmail.com>
2021-06-20 14:07:57 +0500

    game page and reg form open/close

--- app.ts
+++ app.ts
@@ -1,18 +1,180 @@
+//importing
 import { setTemplate, renderPage } from "./src/routing.js";
+import UsersStore, { User } from "./src/UsersStore.js";
+
+//interfaces
+declare global {
+  interface Window {
+    timer: number;
+  }
+}
+
+interface Turns {
+  correct: number;
+  incorrect: number;
+  score: () => number;
+}
+
+interface Cards {
+  turnedNumber: number;
+  turnedList: NodeListOf<Element> | [];
+  pairsNumber: number;
+}
 
 //when application has started it renders about page
 setTemplate();
 renderPage("about");
 
+let gridSize: number;
+
 //other page navigations
 document.querySelectorAll(".page-render").forEach((btn) => {
+  initUI();
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-    renderPage(btn.dataset.value);
+    const page = (btn as HTMLElement).dataset.value;
+    if (page === "about" || page === "ranking" || page === "settings" || page === "game" || page === "reg")
+      if (page !== "game") {
+        renderPage(page);
+        if (window.timer) clearInterval(window.timer);
+      } else if (page === "game") {
+        gridSize =
+          document.getElementById("difficulty") && (<HTMLInputElement>document.getElementById("difficulty")).value
+            ? parseInt((<HTMLInputElement>document.getElementById("difficulty")).value)
+            : 4;
+
+        renderPage("game", {
+          gridSize: gridSize,
+          cardType:
+            document.getElementById("card-type") && (<HTMLInputElement>document.getElementById("card-type")).value
+              ? (<HTMLInputElement>document.getElementById("card-type")).value
+              : "animal",
+        });
+
+        startGame();
+      }
+
     document.querySelectorAll(".navbar__item").forEach((nav) => {
       nav.classList.remove("active");
     });
-    e.currentTarget.closest("div").classList.add("active");
+    btn.closest("div").classList.add("active");
     // window.clearInterval(window.timer);
   });
 });
+
+//registration
+document.getElementById("register").addEventListener("click", () => {
+  renderPage("reg");
+
+  document.getElementById("cancel-reg").addEventListener("click", () => {
+    document.getElementById("app").removeChild(document.getElementById("reg-window"));
+  });
+});
+
+//game operation and logic
+function startGame() {
+  let cards: Cards = {
+    turnedNumber: 0,
+    turnedList: [],
+    pairsNumber: (gridSize * gridSize) / 2,
+  };
+
+  let turns: Turns = {
+    correct: 0,
+    incorrect: 0,
+    score() {
+      return (this.correct + this.incorrect - this.incorrect) * 100 - secondsSpent * 10 > 0
+        ? (this.correct + this.incorrect - this.incorrect) * 100 - secondsSpent * 10
+        : 0;
+    },
+  };
+
+  const userStore = new UsersStore();
+  let currentUser: User = JSON.parse(localStorage.getItem("currentUser"));
+  let secondsSpent: number = 0;
+
+  function checkIfComplete() {
+    console.log("worked");
+
+    if (cards.pairsNumber > turns.correct) {
+      return;
+    } else if (cards.pairsNumber == turns.correct) {
+      clearInterval(window.timer);
+      currentUser.score += turns.score();
+      userStore._updateScore(currentUser);
+      localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+      if (confirm(`You have completed the game. Your score is: ${turns.score()} Would you like to play once more?`)) {
+        document.getElementById("start").click();
+      } else {
+        document.getElementById("page-rank").click();
+      }
+    }
+  }
+
+  document.querySelectorAll(".game__card").forEach((card) => {
+    card.addEventListener("click", () => {
+      if (cards.turnedNumber < 2) {
+        (cards.turnedList as Array<Node>).push(card);
+        card.classList.add("turned");
+        cards.turnedNumber++;
+
+        if (cards.turnedNumber == 2 && (cards.turnedList[0] as HTMLElement).dataset.id != (cards.turnedList[1] as HTMLElement).dataset.id) {
+          cards.turnedList.forEach((card) => {
+            card.classList.add("incorrect");
+          });
+
+          setTimeout(() => {
+            document.querySelectorAll(".game__card:not(.correct)").forEach((card) => {
+              card.classList.remove("turned");
+            });
+
+            cards.turnedList.forEach((card) => {
+              card.classList.remove("incorrect");
+            });
+
+            cards.turnedNumber = 0;
+            cards.turnedList = [];
+          }, 1000);
+          turns.incorrect++;
+        } else if (cards.turnedNumber == 2 && (cards.turnedList[0] as HTMLElement).dataset.id == (cards.turnedList[1] as HTMLElement).dataset.id) {
+          cards.turnedList.forEach((card) => {
+            card.classList.add("correct");
+          });
+
+          cards.turnedNumber = 0;
+          cards.turnedList = [];
+
+          turns.correct++;
+          setTimeout(() => {
+            checkIfComplete();
+          }, 100);
+        }
+      }
+    });
+  });
+
+  let startTime = Date.now();
+  window.timer = window.setInterval(function () {
+    let delta = Date.now() - startTime; // milliseconds elapsed since start
+    secondsSpent = Math.floor(delta / 1000);
+    // alternatively just show wall clock time:
+    (document.querySelector(".game__timer") as HTMLElement).innerText = `${new Date(delta).getMinutes() < 10 ? "0" : ""}${new Date(
+      delta
+    ).getMinutes()}:${new Date(delta).getSeconds() < 10 ? "0" : ""}${new Date(delta).getSeconds()}`;
+  }, 100);
+}
+
+function initUI() {
+  if (localStorage.getItem("currentUser") == null) {
+    document.querySelectorAll(".signed-in").forEach((item) => {
+      (item as HTMLElement).style.display = "none";
+    });
+    (document.querySelector(".not-signed-in") as HTMLElement).style.display = "inline-block";
+  } else {
+    document.querySelectorAll(".signed-in").forEach((item) => {
+      (item as HTMLElement).style.display = "inline-block";
+    });
+    (document.querySelector(".not-signed-in") as HTMLElement).style.display = "none";
+  }
+}
--- src/routing.ts
+++ src/routing.ts
@@ -20,7 +20,7 @@ export function setTemplate() {
             </a>
           </div>
           <div class="navbar__item">
-            <a href="#" class="page-render" data-value="ranking">
+            <a href="#" class="page-render" id="page-rank" data-value="ranking">
               <svg>
                 <use xlink:href="/fonts/symbol-defs.svg#i-star"></use>
               </svg>
@@ -37,60 +37,19 @@ export function setTemplate() {
           </div>
         </nav>
         <div class="header__item">
-          <a href="#" class="btn btn--ghost page-render" data-value="registration" id="register">Register New Player</a>
-          <a href="#" class="btn btn--ghost page-render" data-value="game" id="start">Start Game</a>
-          <a href="#" class="btn btn--ghost" id="quit">Quit</a>
-          <span class="profile-avatar">
+          <a href="#" class="btn btn--ghost page-render not-signed-in" data-value="registration" id="register">Register New Player</a>
+          <a href="#" class="btn btn--ghost page-render signed-in" data-value="game" id="start">Start Game</a>
+          <a href="#" class="btn btn--ghost signed-in" id="quit">Quit</a>
+          <span class="profile-avatar signed-in">
             <img src="/img/profile.png" alt="User Avatar">
           </span>
         </div>
       </header>
       <main id="main"></main>
-      <section class="reg" id="reg">
-        <!-- add "active" class-name to make it visible -->
-        <div class="reg__window">
-          <h1 class="h1">Register new Player</h1>
-          <div class="reg__form">
-            <div class="reg__form--main" action="#">
-              <label for="first-name" class="reg__label">
-                First Name
-                <input type="text" id="first-name" name="first-name" class="reg__input">
-                <svg class="visible">
-                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
-                </svg>
-              </label>
-              <label for="last-name" class="reg__label">
-                Last Name
-                <input type="text" id="last-name" name="last-name" class="reg__input">
-                <svg class="visible">
-                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
-                </svg>
-              </label>
-              <label for="email" class="reg__label">
-                E-mail
-                <input type="email" id="email" name="email" class="reg__input">
-                <svg class="visible">
-                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
-                </svg>
-              </label>
-            </div>
-            <div class="reg__form--avatar">
-              <label for="avatar">
-                <input type="file" id="avatar" class="reg__input" accept="image/png, image/jpg, image/jpeg">
-                <img src="/img/profile.png" id="avatar-image" alt="Profile Icon">
-              </label>
-            </div>
-            <div class="reg__buttons">
-              <a href="#" class="btn btn--dark disabled" id="add-user">Add User</a>
-              <a href="#" class="btn btn--light" id="cancel-reg">Cancel</a>
-            </div>
-          </div>
-        </div>
-      </section>
   `;
 }
 
-export function renderPage(page: "about" | "ranking" | "settings" | "game", gameSettings?: { gridSize: 4; cardType: "animal" }): void {
+export function renderPage(page: "about" | "ranking" | "settings" | "game" | "reg", gameSettings?: { gridSize: number; cardType: string }): void {
   let content: string = ``;
 
   if (page === "about") {
@@ -121,7 +80,7 @@ export function renderPage(page: "about" | "ranking" | "settings" | "game", game
           </main>
       `;
   } else if (page === "ranking") {
-    const users = new UsersStore()._getAllUsers();
+    let users = new UsersStore()._getAllUsers();
 
     document.getElementById("main").innerHTML = `
         <main class="rank">
@@ -133,11 +92,12 @@ export function renderPage(page: "about" | "ranking" | "settings" | "game", game
       `;
 
     setTimeout(() => {
-      const sortedUsers = users.sort(function (x, y) {
-        return y.score - x.score;
-      });
-      users.forEach((user) => {
-        document.getElementById("rank-list").innerHTML += `
+      users
+        .sort(function (x, y) {
+          return y.score - x.score;
+        })
+        .forEach((user) => {
+          document.getElementById("rank-list").innerHTML += `
           <li class="rank__item">
             <div class="rank__item-container">
               <img src="${user.avatar ? user.avatar : " /img/profile.png"}" alt="User 1" />
@@ -148,7 +108,7 @@ export function renderPage(page: "about" | "ranking" | "settings" | "game", game
               </div>
             <h3 class="rank__item-score">Score: <span>${user.score}</span></h3>
           </li>`;
-      });
+        });
     }, 1000);
   } else if (page === "settings") {
     document.getElementById("main").innerHTML = `
@@ -193,15 +153,25 @@ export function renderPage(page: "about" | "ranking" | "settings" | "game", game
       return cardIndexes.sort((a, b) => 0.5 - Math.random());
     };
 
-    document.getElementById("main").innerHTML =
-      `
+    document.getElementById("main").innerHTML = `
         <main class="game">
           <div class="game__container">
             <div class="game__timer">00:00</div>
-            <div class="game__cards">
-              ` +
-      gameModeSetup().forEach((i) => {
-        return `
+            <div class="game__cards" id="game-cards"></div>
+          </div>
+        </main>
+      `;
+
+    document.getElementById("game-cards").setAttribute(
+      "style",
+      `
+      grid-template-columns: repeat(${gameSettings.gridSize}, 15%);
+      grid-template-rows: repeat(${gameSettings.gridSize}, 15%);
+    `
+    );
+
+    gameModeSetup().forEach((i) => {
+      document.getElementById("game-cards").innerHTML += `
               <div class="game__card" data-id="photo${i}">
                 <div class="game__card--front">
                   <img src="/img/mask.png" alt="Mask">
@@ -211,11 +181,56 @@ export function renderPage(page: "about" | "ranking" | "settings" | "game", game
                 </div>
               </div>
             `;
-      }) +
-      `
+    });
+  } else if (page === "reg") {
+    document.getElementById("app").insertAdjacentHTML(
+      
+      "beforeend",
+ 
+          `
+      <section class="reg" id="reg-window">
+        <!-- add "active" class-name to make it visible -->
+        <div class="reg__window">
+          <h1 class="h1">Register new Player</h1>
+          <div class="reg__form">
+            <div class="reg__form--main" action="#">
+              <label for="first-name" class="reg__label">
+                First Name
+                <input type="text" id="first-name" name="first-name" class="reg__input">
+                <svg class="visible">
+                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
+                </svg>
+              </label>
+              <label for="last-name" class="reg__label">
+                Last Name
+                <input type="text" id="last-name" name="last-name" class="reg__input">
+                <svg class="visible">
+                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
+                </svg>
+              </label>
+              <label for="email" class="reg__label">
+                E-mail
+                <input type="email" id="email" name="email" class="reg__input">
+                <svg class="visible">
+                  <use xlink:href="/fonts/symbol-defs.svg#i-tick"></use>
+                </svg>
+              </label>
+            </div>
+            <div class="reg__form--avatar">
+              <label for="avatar">
+                <input type="file" id="avatar" class="reg__input" accept="image/png, image/jpg, image/jpeg">
+                <img src="/img/profile.png" id="avatar-image" alt="Profile Icon">
+              </label>
+            </div>
+            <div class="reg__buttons">
+              <a href="#" class="btn btn--dark disabled" id="add-user">Add User</a>
+              <a href="#" class="btn btn--light" id="cancel-reg">Cancel</a>
             </div>
           </div>
-        </main>
-      `;
+        </div>
+      </section>
+    `
+    
+    );
   }
 }
